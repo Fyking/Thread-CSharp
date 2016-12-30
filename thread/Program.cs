8cd0a8ec0cf7fc2a784a24186cb463b995054ed7
Fyking <kang853212441@163.com>
2016-12-30 11:27:37 +0800

    thread

---
+++ thread/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace thread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("start");
+            Thread th = new Thread(delegate() {
+                for (int i = 0; i < 100; i++)
+                {
+                    Console.WriteLine(i);
+                    Thread.Sleep(100);
+                }
+            });
+            th.Start();
+            for (int i = 100; i < 200; i++)
+            {
+                Console.WriteLine(i);
+                Thread.Sleep(100);
+            }
+            Console.WriteLine("end");
+            Console.ReadKey();
+        }
+    }
+}
